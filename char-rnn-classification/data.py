b8b5f8e928ac6f668c2b62f2923cb66223561295
Sean Robertson <sprobertson@gmail.com>
2017-01-24 01:11:55 -0800

    check chars are in all_letters

--- char-rnn-classification/data.py
+++ char-rnn-classification/data.py
@@ -3,33 +3,34 @@ import glob
 import unicodedata
 import string
 
-filenames = glob.glob('../data/names/*.txt')
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters)
+
+def findFiles(path): return glob.glob(path)
 
 # Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
-def stripAccents(s):
+def unicodeToAscii(s):
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
         if unicodedata.category(c) != 'Mn'
+        and c in all_letters
     )
 
 # Read a file and split into lines
 def readLines(filename):
     lines = open(filename).read().strip().split('\n')
-    return [stripAccents(line) for line in lines]
+    return [unicodeToAscii(line) for line in lines]
 
 # Build the category_lines dictionary, a list of lines per category
 category_lines = {}
 all_categories = []
-for filename in filenames:
+for filename in findFiles('../data/names/*.txt'):
     category = filename.split('/')[-1].split('.')[0]
     all_categories.append(category)
     lines = readLines(filename)
     category_lines[category] = lines
 
 n_categories = len(all_categories)
-
-all_letters = string.ascii_letters
-n_letters = len(all_letters)
 
 # Find letter index from all_letters, e.g. "a" = 0
 def letterToIndex(letter):
